b91c19646d583c8e22c993dcf5eb200a8597c165
viylouu <131220600+viylouu@users.noreply.github.com>
2025-09-23 16:13:20 -0600

    formatting

--- src/main.c
+++ src/main.c
@@ -4,23 +4,13 @@
 #include <core/renderer/renderer.h>
 
 void init(void) {}
-
-void update(void) {
-    //printf("checking key %d\n", XKB_KEY_A);
-
-    printf("%d ", (s32)(1.f/cc_delta));
-    if (cc_isKeyHeld(XKB_KEY_A))
-        printf("key a pressed\n");
-}
-
-#include <GL/gl.h>
+void update(void) {}
 
 void render(void) {
-    glClearColor(.2,.3,.4,1);
-    glClear(GL_COLOR_BUFFER_BIT);
+    cc_rendererClear(.2,.3,.4,1);
 
-    cc_gl_rendererSetTint(1,0,0,1);
-    cc_gl_rendererDrawRect(0,0,64,64);
+    cc_rendererSetTint(1,0,0,1);
+    cc_rendererDrawRect(0,0,64,64);
 }
 
 void clean(void) {}
